30345469ede4f043bfcc78b152253473d0f66c46
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-12-04 19:17:49 +0500

    refactor server.js: clean up code by removing unused file system checks and logging

--- server/server.js
+++ server/server.js
@@ -1,21 +1,14 @@
+require("dotenv").config();
 const { GoogleAIFileManager } = require("@google/generative-ai/server");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
-const fs = require("fs");
 const path = require("path");
-
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
 const port = 5000;
-
-const dir = path.join(__dirname, "images");
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -43,9 +36,6 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         displayName: req.file.originalname,
       }
     );
-    console.log(
-      `Uploaded file ${uploadResult.file.displayName} as: ${uploadResult.file.uri}`
-    );
 
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
@@ -58,22 +48,12 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         },
       },
     ]);
-    console.log(result.response.text());
     res.send(result.response.text());
   } catch (error) {
-    console.error("Error processing the request:", error);
     res.status(500).send("An error occurred while processing the request.");
   }
 });
 
-app.use((error, req, res, next) => {
-  if (error instanceof multer.MulterError) {
-    console.error("Multer Error:", error.field);
-    return res.status(400).send(`Multer Error: ${error.message}`);
-  }
-  next(error);
-});
-
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on http://localhost:${port}`);
 });
